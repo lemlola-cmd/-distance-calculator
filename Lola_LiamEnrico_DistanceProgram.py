909bca4ad300b42c0ef57a51c52261000492aa15
bluisnotlucky <lemlola@brc.pshs.edu.ph>
2026-08-14 10:01:15 +0800

    Round distance output and enhance comments
    Updated the distance calculation output to round the result to two decimal places and added comments for clarity.

--- Lola_LiamEnrico_DistanceProgram.py
+++ Lola_LiamEnrico_DistanceProgram.py
@@ -1,11 +1,15 @@
 import math
+# This imports the math library so that I can use sqrt.() and pow.()
 
 x1 = float(input("Enter x1:"))
 y1 = float(input("Enter y1:"))
 x2 = float(input("Enter x2:"))
 y2 = float(input("Enter y2:"))
 
-distance = math.sqrt(math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2))
-print("the distance between the two points is:", distance)
+# This is where you input the x and y values.
 
-# Using a library is more practical and easier than making something from scratch because, making something from scratch is long and tiresome, having to go to trial and error if you get it wrong. 
+distance = math.sqrt(math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2))
+print("the distance between the two points is:", round(distance, 2))
+
+# This computes the distance and prints it.
+
